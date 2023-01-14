df7892ff71a466f0248331f3e04906f2c08aaeeb
Zhserg28 <serg280880@gmail.com>
2023-01-15 01:20:11 +0800

    refactoring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,6 @@ import React,{useState} from "react";
 const Counter = () => {
     //let count = 0;
     const [count,setCount] = useState(0)
-    const [tags, setTags] = useState(['tag1','tag2','tag3'])
         
     const formatCount=()=>{
         return count===0 ? "empty" : count
@@ -19,25 +18,6 @@ const Counter = () => {
     };
     const handleDecrement=()=>{
         setCount((prevState)=>prevState - 1);
-    }
-    const handleTagChange=(id)=>{
-        setTags(prevState=>prevState.filter(tag=>tag!==id))
-    }
-    const renderTags=()=>{
-        return tags.length!==0
-            && tags.map((tag)=>(
-            <li 
-                key={tag} 
-                className="btn btn-primary btn-sm m-2" 
-                onClick={()=>handleTagChange(tag)}
-            >
-                {tag}
-            </li>
-            ))
-            
-    };
-    if(tags.length!==0){
-        return <ul>{renderTags()}</ul>;
     }
  
     return (
